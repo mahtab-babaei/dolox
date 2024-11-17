b8ce35ef4fc41d001e4a8d5456f5354edb2d6a40
Mahtab <mahtaab.babaei@gmail.com>
2024-11-17 13:29:50 +0330

    Refactor: Fix some issue style of login and register page

--- app/login/LoginSection.jsx
+++ app/login/LoginSection.jsx
@@ -1,56 +1,21 @@
 "use client";
+import { eyeIcon, eyeSlashIcon } from "@/utils/constants";
+import { loginReq } from "@/utils/Request"; // Assuming this is your login request function
 import { ImageURL } from "@/utils/URL";
+import { useFormik } from "formik";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { loginReq } from "@/utils/Request"; // Assuming this is your login request function
-import { setCookie } from "nookies"; // Import nookies for setting cookies
 import { useRouter } from "next/navigation";
+import { setCookie } from "nookies"; // Import nookies for setting cookies
+import { useState } from "react";
+import * as Yup from "yup";
+import ErrorMessage from "../components/global/ErrorMessage";
 
 const LoginSection = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false); // Loading state
   const [message, setMessage] = useState(""); // Message state
   const [showPassword, setShowPassword] = useState(false); // Password visibility state
-
-  // Eye icon SVGs for toggling password visibility
-  const eyeIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <path
-        d="M12 9C11.2044 9 10.4413 9.31607 9.87868 9.87868C9.31607 10.4413 9 11.2044 9 12C9 12.7956 9.31607 13.5587 9.87868 14.1213C10.4413 14.6839 11.2044 15 12 15C12.7956 15 13.5587 14.6839 14.1213 14.1213C14.6839 13.5587 15 12.7956 15 12C15 11.2044 14.6839 10.4413 14.1213 9.87868C13.5587 9.31607 12.7956 9 12 9ZM12 17C10.6739 17 9.40215 16.4732 8.46447 15.5355C7.52678 14.5979 7 13.3261 7 12C7 10.6739 7.52678 9.40215 8.46447 8.46447C9.40215 7.52678 10.6739 7 12 7C13.3261 7 14.5979 7.52678 15.5355 8.46447C16.4732 9.40215 17 10.6739 17 12C17 13.3261 16.4732 14.5979 15.5355 15.5355C14.5979 16.4732 13.3261 17 12 17ZM12 4.5C7 4.5 2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C17 19.5 21.27 16.39 23 12C21.27 7.61 17 4.5 12 4.5Z"
-        className="fill-base-content"
-      />
-    </svg>
-  );
-
-  const eyeSlashIcon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <path
-        d="M12 9C11.2044 9 10.4413 9.31607 9.87868 9.87868C9.31607 10.4413 9 11.2044 9 12C9 12.7956 9.31607 13.5587 9.87868 14.1213C10.4413 14.6839 11.2044 15 12 15C12.7956 15 13.5587 14.6839 14.1213 14.1213C14.6839 13.5587 15 12.7956 15 12C15 11.2044 14.6839 10.4413 14.1213 9.87868C13.5587 9.31607 12.7956 9 12 9ZM12 17C10.6739 17 9.40215 16.4732 8.46447 15.5355C7.52678 14.5979 7 13.3261 7 12C7 10.6739 7.52678 9.40215 8.46447 8.46447C9.40215 7.52678 10.6739 7 12 7C13.3261 7 14.5979 7.52678 15.5355 8.46447C16.4732 9.40215 17 10.6739 17 12C17 13.3261 16.4732 14.5979 15.5355 15.5355C14.5979 16.4732 13.3261 17 12 17ZM12 4.5C7 4.5 2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C17 19.5 21.27 16.39 23 12C21.27 7.61 17 4.5 12 4.5Z"
-        className="fill-base-content"
-      />
-      <path
-        d="M18 5L6 19"
-        className="stroke-base-content"
-        stroke-width="3"
-        stroke-linecap="round"
-      />
-    </svg>
-  );
 
   const formik = useFormik({
     initialValues: {
@@ -122,14 +87,12 @@ const LoginSection = () => {
                   dir="ltr"
                   type="text"
                   placeholder="0922222222"
-                  className="input focus:outline-primary border-none w-full max-w-xs bg-base-200 font-vazir"
+                  className="input focus:outline-secondary border-none w-full max-w-xs bg-base-200 font-vazir"
                   {...formik.getFieldProps("phonenumber")}
                   disabled={loading} // Disable input while loading
                 />
                 {formik.touched.phonenumber && formik.errors.phonenumber ? (
-                  <div className="text-red-500 text-sm pt-1">
-                    {formik.errors.phonenumber}
-                  </div>
+                  <ErrorMessage>{formik.errors.phonenumber}</ErrorMessage>
                 ) : null}
               </label>
 
@@ -142,7 +105,7 @@ const LoginSection = () => {
                   dir="ltr"
                   type={showPassword ? "text" : "password"}
                   placeholder="رمز خود را وارد کنید"
-                  className="input focus:outline-primary w-full max-w-xs bg-base-200 font-vazir"
+                  className="input focus:outline-secondary border-none w-full max-w-xs bg-base-200 font-vazir"
                   {...formik.getFieldProps("password")}
                   disabled={loading} // Disable input while loading
                 />
@@ -156,16 +119,17 @@ const LoginSection = () => {
                 </button>
 
                 {formik.touched.password && formik.errors.password ? (
-                  <div className="text-red-500 text-sm pt-1">
-                    {formik.errors.password}
-                  </div>
+                  <ErrorMessage>{formik.errors.password}</ErrorMessage>
                 ) : null}
               </label>
 
               <p className="text-xs font-vazir text-gray-400 mt-2">{message}</p>
               <p className="text-xs font-vazir text-gray-400 mt-2">
                 هنوز ثبت نام نکردید؟
-                <Link href="/register" className="text-primary px-2 font-vazir">
+                <Link
+                  href="/register"
+                  className="text-secondary px-2 font-vazir"
+                >
                   ثبت نام
                 </Link>
               </p>
@@ -173,7 +137,7 @@ const LoginSection = () => {
               <div className="flex justify-between items-center">
                 <button
                   type="submit"
-                  className="btn bg-primary border-none text-white !min-h-1 h-10 mt-2 disabled:bg-gray-400"
+                  className="btn bg-secondary border-none text-white !min-h-1 h-10 mt-2 disabled:bg-gray-400"
                   disabled={loading}
                 >
                   {loading ? (
@@ -184,9 +148,9 @@ const LoginSection = () => {
                 </button>
                 <Link
                   href="/forgetpassword"
-                  className="text-primary px-2 font-vazir text-xs"
+                  className="text-secondary px-2 font-vazir text-xs"
                 >
-                  فراموش رمز
+                  فراموشی رمز
                 </Link>
               </div>
             </form>
