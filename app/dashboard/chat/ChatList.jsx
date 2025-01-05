ec0b324edaef1df92d1400481e17b1d4e75a31ee
Mahtab <mahtaab.babaei@gmail.com>
2025-01-05 14:36:24 +0330

    Create ChatList component

---
+++ app/dashboard/chat/ChatList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatList = () => {
+  return (
+    <div className="w-1/4 h-screen bg-white rounded-3xl p-4">
+      <h2 className="text-lg text-center font-bold mb-4">پیام ها</h2>
+    </div>
+  );
+};
+
+export default ChatList;
--- app/dashboard/chat/page.js
+++ app/dashboard/chat/page.js
@@ -4,6 +4,7 @@ import ChatRoom from "./ChatRoom";
 import { useChatDataStore } from "@/stores/useChatDataStore";
 import { useSearchParams } from "next/navigation";
 import { joinChatRoom } from "@/utils/Requests";
+import ChatList from "./ChatList";
 
 const ChatPage = () => {
   const chatData = useChatDataStore((state) => state.chatData);
@@ -45,7 +46,7 @@ const ChatPage = () => {
 
   return (
     <div className="flex justify-start h-fit bg-base-200 w-full pt-40 pb-10 px-4 gap-8">
-      <div className="w-1/4 h-screen bg-white rounded-3xl p-4">list</div>
+      <ChatList />
       <ChatRoom roomName={chatData.roomName[0]} username={chatData.username} />
     </div>
   );
