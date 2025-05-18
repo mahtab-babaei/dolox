7d5334403c09259178bb20c6953d4111dd94aeb3
Mahtab <mahtaab.babaei@gmail.com>
2025-05-18 12:35:21 +0330

    Fix the problem of favorites list

--- app/dashboard/favorites/FavoritesList.jsx
+++ app/dashboard/favorites/FavoritesList.jsx
@@ -2,11 +2,14 @@
 import AdsItem from "@/app/components/global/AdsItem";
 import { fetchAdsByFilter } from "@/utils/Requests";
 import React, { useEffect, useState } from "react";
+import { fetchFavoritesList } from "@/utils/Requests";
 
-const FavoritesList = ({ list }) => {
+const FavoritesList = () => {
   const [favoritesList, setFavoritesList] = useState([]);
   useEffect(() => {
     const fetchFavorites = async () => {
+      const list = await fetchFavoritesList();
+
       const totalAds = await fetchAdsByFilter({
         brand: "",
         city: "همه شهر ها",
@@ -17,25 +20,26 @@ const FavoritesList = ({ list }) => {
         order: "جدیدترین",
       });
       const favorites = totalAds.data.results.filter((ad) =>
-        list.some((entry) => entry.car === ad.id)
+        list?.data?.some((entry) => entry.car === ad.id)
       );
       setFavoritesList(favorites);
     };
     fetchFavorites();
-  });
+  }, []);
+
   return (
     <div className="h-full w-full px-4">
       <div className="bg-white w-full max-w-screen-lg h-fit rounded-[34px] p-4 base:p-8 flex flex-col">
         <h1 className="text-xl text-black mb-6">آگهی های مورد علاقه</h1>
         <div className="grid grid-cols-1 base:grid-cols-2 gap-4">
-        {favoritesList.map((favorite) => (
-          <AdsItem
-            key={favorite.id}
-            ad={favorite}
-            bgColor="white"
-            fillColor="black"
-          />
-        ))}
+          {favoritesList.map((favorite) => (
+            <AdsItem
+              key={favorite.id}
+              ad={favorite}
+              bgColor="white"
+              fillColor="black"
+            />
+          ))}
         </div>
       </div>
     </div>
--- app/dashboard/favorites/page.js
+++ app/dashboard/favorites/page.js
@@ -1,14 +1,12 @@
 import React from "react";
 import DashboardPanel from "../DashboardPanel";
 import FavoritesList from "./FavoritesList";
-import { fetchFavoritesList } from "@/utils/Requests";
 
-const FavoritesPage = async () => {
-  const list = await fetchFavoritesList();
+const FavoritesPage = () => {
   return (
     <div className="flex justify-start h-fit bg-base-200 w-full pt-40 pb-10">
       <DashboardPanel />
-      <FavoritesList list={list.data} />
+      <FavoritesList />
     </div>
   );
 };
